32a656ac929ab48701432f003d3452b20af2c0f7
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-15 11:54:06 -0800

    Update bit_operations.c

--- bit_operations.c
+++ bit_operations.c
@@ -5,31 +5,34 @@ unsigned getbits(unsigned x, int p, int n) {
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y ) {	//Exercise 2.6
-	x = x & ~(~(~0 << n)) << (++p - n);
-	y = (y & ~(~0 << n)) << (++p - n);
-	x = x | y;
-	return x;
+	unsigned mask = ~(~0 << n);
+	unsigned shifted_mask = mask << (p + 1 - n);
+	return ((y&mask) << (p + 1 - n)) | (x& ~shifted_mask);
 }
 
-unsigned invert(unsigned x,int p, int n) {		//Exercise 2.7
-	x = x ^ (~(~0 << n) << (++p - n));
-	return x;
+unsigned invert(unsigned x, int p, int n) {		//Exercise 2.7
+	unsigned mask = ~(~0 << n);
+	unsigned shifted_mask = mask << (p + 1 - n);
+	return (x & ~shifted_mask) | (~x & shifted_mask);
 }
 
 unsigned rightrot(unsigned x, int n) {	//Exercise 2.8
-	x = x & ~(~0 >> n) << (n + 1);
-	return x;
+	unsigned y = x >> n;
+	unsigned mask = ~(~0 << n);
+	unsigned not_mask = ~mask;
+	return (y & not_mask) | (x & mask);
 }
 
 int main() {
 	unsigned x = ~0;
 	unsigned y = getbits(x, 3, 1);
-	printf("getbits x is: %u and y is: %d\n", x, y);
 	y = setbits(x, 3, 1, y);
-	printf("setbits x is: %u and y is: %u\n",x, y);
+	printf("setbits: x is: %u and y is: %u\n",x, y);
+	
 	y = invert(x, 3, 1);
-	printf("invert x is: %u and y is: %u\n", x, y);
+	printf("invert: x is: %u and y is: %u\n", x, y);
+
 	y = rightrot(x, 1);
-	printf("rightrot x is: %u and y is: %u\n", x, y);
+	printf("rightrot: x is: %u and y is: %u\n", x, y);
 	return 0;
 }
